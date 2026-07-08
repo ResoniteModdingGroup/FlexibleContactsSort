49a4fc3d3db33434b4535aea1e2419c97936c773
Arne Kiesewetter <banane9@live.de>
2026-07-09 01:32:54 +0200

    Overhaul config with new features

--- FlexibleContactsSort/SessionCapacityConfig.cs
+++ FlexibleContactsSort/SessionCapacityConfig.cs
@@ -1,19 +1,32 @@
 ﻿using Elements.Core;
 using FrooxEngine;
 using MonkeyLoader.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MonkeyLoader.Resonite.Configuration;
 
 namespace FlexibleContactsSort
 {
     internal sealed class SessionCapacityConfig : ConfigSection
     {
-        private readonly DefiningConfigKey<colorX> _emptySessionColorKey = new("EmptySessionColor", "Color of the user count when only the host is there.", () => RadiantUI_Constants.Hero.GREEN);
-        private readonly DefiningConfigKey<colorX> _fullSessionColorKey = new("FullSessionColor", "Color of the user count when the session is full.", () => RadiantUI_Constants.Hero.RED);
-        private readonly DefiningConfigKey<bool> _showUsageLevelWithColorGradientKey = new("ShowUsageLevelWithColorGradient", "Color the user count based on capacity usage.", () => true);
+        private static readonly ConfigKeySubgroup _gradientSubgroup = new(-10, "Gradient");
+
+        private readonly DefiningConfigKey<colorX> _emptySessionColorKey = new("EmptySessionColor", "Color of the user count when only the host is there.", () => RadiantUI_Constants.Hero.GREEN)
+        {
+            _gradientSubgroup,
+            new ConfigKeyPriority(5)
+        };
+
+        private readonly DefiningConfigKey<colorX> _fullSessionColorKey = new("FullSessionColor", "Color of the user count when the session is full.", () => RadiantUI_Constants.Hero.RED)
+        {
+            _gradientSubgroup,
+            new ConfigKeyPriority(1)
+        };
+
+        private readonly DefiningConfigKey<bool> _showUsageLevelWithColorGradientKey = new("ShowUsageLevelWithColorGradient", "Color the user count based on capacity usage.", () => true)
+        {
+            _gradientSubgroup,
+            new ConfigKeyPriority(10)
+        };
+
         private readonly DefiningConfigKey<bool> _showUserCapacityInSessionListKey = new("ShowUserCapacityInSessionList", "Show the user capacity of contacts' joinable sessions.", () => true);
         public override string Description => "Contains options for how to highlight contacts' session capacity.";
         public colorX EmptySessionColor => _emptySessionColorKey.GetValue();
@@ -23,5 +36,11 @@ namespace FlexibleContactsSort
         public bool ShowUsageLevelWithColorGradient => _showUsageLevelWithColorGradientKey.GetValue();
         public bool ShowUserCapacityInSessionList => _showUserCapacityInSessionListKey.GetValue();
         public override Version Version { get; } = new(1, 0, 0);
+
+        public SessionCapacityConfig()
+        {
+            _emptySessionColorKey.Components.Add(new ConfigKeyEnabledSource<colorX>(_showUsageLevelWithColorGradientKey));
+            _fullSessionColorKey.Components.Add(new ConfigKeyEnabledSource<colorX>(_showUsageLevelWithColorGradientKey));
+        }
     }
 }
